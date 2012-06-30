a2068d6867aa04cd641c82974c69375a3cce7ece
jasonchi <jasonchi@live.cn>
2012-06-30 11:51:33 +0800

    commit the code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JuggernautChat::Application.config.secret_token = '166b773a0d4425bd21ffe1786ce6474f5e6ada110457cf3ff24a61ff90b9b71f8f2498f4b51aba60d4883df61b214de058fa246b8f167854f49db8bc89e8153c'
